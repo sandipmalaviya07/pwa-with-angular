30ce70ca5a2a3dc3b74aeffc3adf1a69f2f9dc49
Ajay Thakor <ajaykumar.thakor@samarpaninfotech.co.in>
2022-12-30 16:07:34 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-pwa-demo';
+  textareaValue:string = "";
+
+  constructor() {
+    this.textareaValue = <any>localStorage.getItem('textareaValue');
+  }
+
+  save() {
+    console.log(this.textareaValue);
+    localStorage.setItem('textareaValue', this.textareaValue);
+  }
+
+  clear() {
+    localStorage.removeItem('textareaValue')
+    this.textareaValue = "";
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
